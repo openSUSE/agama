9a7f220ee5bc785871add913d83ef0f5209fc420
Imobach González Sosa <igonzalezsosa@suse.com>
2024-01-19 10:08:16 +0000

    feat(server): add a /ping endpoint

--- rust/agama-web-server/src/main.rs
+++ rust/agama-web-server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Json, Router};
+use serde_json::{json, Value};
+
+async fn ping() -> Json<Value> {
+    Json(json!({ "status": "ok" }))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/ping", get(ping));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:9091").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
